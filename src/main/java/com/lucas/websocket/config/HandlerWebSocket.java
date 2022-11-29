8ebf94972d72a4e6122d02ace173632b3a37a856
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2022-11-29 19:35:52 -0300

    Initial Commit

---
+++ src/main/java/com/lucas/websocket/config/HandlerWebSocket.java
@@ -0,0 +1,38 @@
+package com.lucas.websocket.config;
+
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class HandlerWebSocket extends TextWebSocketHandler {
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+        session.sendMessage(new TextMessage("Salve! Voce me mandou isso " + message.getPayload() + "?"));
+    }
+
+    @Override
+    protected void handlePongMessage(WebSocketSession session, PongMessage message) throws Exception {
+        super.handlePongMessage(session, message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return super.supportsPartialMessages();
+    }
+}
